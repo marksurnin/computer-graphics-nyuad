71065a04ff02f2f45c5fdade042c0b27addad470
Mark Surnin <marksurnin@gmail.com>
2016-09-20 22:14:33 +0400

    Make the turle understand F, +, and -.

--- current/assignment_2/L-Systems.js
+++ current/assignment_2/L-Systems.js
@@ -15,10 +15,8 @@ window.onload = function init() {
 			var program = initShaders( gl, "vertex-shader", "fragment-shader" );
 			gl.useProgram( program );
 			
-			
-
 			var initial_config = {
-			     x: 0, y:0, theta: Math.PI/2	
+			  x: 0, y:0, theta: Math.PI/2	
 			};
 
 			var alpha = Math.PI/8;
@@ -26,18 +24,20 @@ window.onload = function init() {
 			var axiom = "F";
 
 			var production_rules = {
-				F: "F[+F]F[-F]F"		  	//axiom:F, alpha = pi/8	
-				//F: "FF+F+F+FF+F+F-F"    	//axiom:F, alpha = pi/2
+				// F: "F[+F]F[-F]F"		  	//axiom:F, alpha = pi/8	
+				// F: "FF+F+F+FF+F+F-F"    	//axiom:F, alpha = pi/2
 				//F: "F+F-F-FF+F+F-F"      	//axiom:F, alpha = pi/2
 				//F: "FF+[+F-F-F]-[-F+F+F]" //axiom:F, alpha = pi/8
 				//F: "FF", X:"F[+X]F[-X]+X" //axiom:X, alpha = pi/9
-
+				F: "F+F-F"    	//axiom:F, alpha = pi/2
 			};
 
 			var num_productions = 5;
 
 			var v = turtle(initial_config, alpha, axiom, 
 			                      production_rules, num_productions);
+
+			// console.log(v);
 
 			// Load data into a buffer
 			var vBuffer = gl.createBuffer();
@@ -49,28 +49,74 @@ window.onload = function init() {
 			gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
 			gl.enableVertexAttribArray(vPosition);
 			
-			//Draw 
+			// Draw 
 			var num_vertices = v.length/2;
-			gl.drawArrays(gl.LINES, 0, num_vertices); 
+			gl.drawArrays(gl.LINE_STRIP, 0, num_vertices); 
 };
 
-function turtle(initial_config, alpha, axiom, 
-			    production_rules, num_productions){
+function turtle(initial_config, alpha, axiom, production_rules, num_productions) {
+	// console.log(initial_config, alpha, axiom, production_rules, num_productions);
+	
 
-	 // your code here
+	// Initialize the points array and append the starting point coordinates.
+	var points = [];
+	points = points.concat(initial_config.x, initial_config.y);
+	// var direction = initial_config.theta;
+
+	// Convert the production_rules string into an array of characters.
+	var instructions = production_rules.F;
+	instructions = instructions.split('');
+
+	var basepoint = clone(initial_config);
+	var statepoint = {};
+
+	instructions.forEach(function(instruction, i) {
+		console.log(instruction, basepoint);
+		var newpoint = {};
+		switch (instruction) {
+			case 'F':
+				newpoint.x = basepoint.x + Math.cos(basepoint.theta);
+				newpoint.y = basepoint.y + Math.sin(basepoint.theta);
+				points.push(newpoint.x, newpoint.y);
+
+				basepoint.x = newpoint.x;
+				basepoint.y = newpoint.y;
+
+				break;
+			case '+':
+				basepoint.theta -= alpha;
+				break;
+			case '-':
+				basepoint.theta += alpha;
+				break;
+			default:
+				return;
+		}
+	});
+
+	// var points = [];
+	// points.push(-0.9,0,0.9,0);
+	console.log(points);
+	points = normalize(points);
+	return points;
 }
 
-
-// below are some functions you may find useful
-
-function clone(obj){
+// Clone a JavaScript object.
+function clone(obj) {
 		return JSON.parse(JSON.stringify(obj));
 }
 
-function normalize(v){
+// Convert between the canvas and WebGL coordinates.
+function normalize(v) {
 	var x = [], y =[], i;
-	for(i=0; i<v.length; i+=2) x.push(v[i]);
-	for(i=1; i<v.length; i+=2) y.push(v[i]);
+
+	for (i=0; i<v.length; i+=2) {
+		x.push(v[i]);
+	}
+	for (i=1; i<v.length; i+=2) {
+		y.push(v[i]);
+	}
+
 	var xmin = Math.min.apply(null,x);
 	var xmax = Math.max.apply(null,x);
 	var ymin = Math.min.apply(null,y);
@@ -80,6 +126,11 @@ function normalize(v){
 	var xrange = xmax - xmin;
 	var yrange = ymax - ymin;
 	var s = 1.9/Math.max(xrange, yrange);
-    for(i=0; i<v.length; i+=2) {v[i] = s*(v[i] - xmid); }
-    for(i=1; i<v.length; i+=2) {v[i] = s*(v[i] - ymid); }
+  for (i=0; i<v.length; i+=2) {
+  	v[i] = s*(v[i] - xmid);
+  }
+  for (i=1; i<v.length; i+=2) {
+  	v[i] = s*(v[i] - ymid);
+  }
+	return v;
 }
