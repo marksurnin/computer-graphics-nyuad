fd9ca3f56bd949a96146b214e11058be7f2e5772
Simon\GitHub_Desktop <simon.seo@nyu.edu>
2016-11-27 21:59:25 +0400

    Reflection mapping: Simple colorful background using cubemap

--- current/assignment_8/NormalMapping.js
+++ current/assignment_8/NormalMapping.js
@@ -60,13 +60,55 @@ window.onload = function init() {
 	gl.uniform3fv( Locations.Is, flatten(Light.Is) );
 
 
+
 	// set up scene	
 
 	obj1 = Cube();
 	obj1.diffuseMap = "cubemap.jpg";
-	// obj1.normalMap = "Textures/earth-normal.jpg";
 	objInit(obj1);
 	obj1.setModelMatrix(rotateX(90));
+
+
+	// function configureCubeMap() {
+		var numVertices  = 36;
+		var texSize = 4;
+		var numChecks = 2;
+
+		var flag = true;
+
+		var red = new Uint8Array([255, 0, 0, 255]);
+		var green = new Uint8Array([0, 255, 0, 255]);
+		var blue = new Uint8Array([0, 0, 255, 255]);
+		var cyan = new Uint8Array([0, 255, 255, 255]);
+		var magenta = new Uint8Array([255, 0, 255, 255]);
+		var yellow = new Uint8Array([255, 255, 0, 255]);
+
+		var cubeMap = gl.createTexture();
+
+
+		gl.activeTexture( gl.TEXTURE3 );
+		gl.uniform1i(gl.getUniformLocation(program, "texMap"), 3);
+	    gl.bindTexture(gl.TEXTURE_CUBE_MAP, cubeMap);
+
+	    gl.texImage2D(gl.TEXTURE_CUBE_MAP_POSITIVE_X ,0,gl.RGBA,
+	       1,1,0,gl.RGBA,gl.UNSIGNED_BYTE, red);
+	    gl.texImage2D(gl.TEXTURE_CUBE_MAP_NEGATIVE_X ,0,gl.RGBA,
+	       1,1,0,gl.RGBA,gl.UNSIGNED_BYTE, green);
+	    gl.texImage2D(gl.TEXTURE_CUBE_MAP_POSITIVE_Y ,0,gl.RGBA,
+	       1,1,0,gl.RGBA,gl.UNSIGNED_BYTE, blue);
+	    gl.texImage2D(gl.TEXTURE_CUBE_MAP_NEGATIVE_Y ,0,gl.RGBA,
+	       1,1,0,gl.RGBA,gl.UNSIGNED_BYTE, cyan);
+	    gl.texImage2D(gl.TEXTURE_CUBE_MAP_POSITIVE_Z ,0,gl.RGBA,
+	       1,1,0,gl.RGBA,gl.UNSIGNED_BYTE, yellow);
+	    gl.texImage2D(gl.TEXTURE_CUBE_MAP_NEGATIVE_Z ,0,gl.RGBA,
+	       1,1,0,gl.RGBA,gl.UNSIGNED_BYTE, magenta);
+
+
+	    gl.texParameteri(gl.TEXTURE_CUBE_MAP,gl.TEXTURE_MAG_FILTER,gl.NEAREST);
+	    gl.texParameteri(gl.TEXTURE_CUBE_MAP,gl.TEXTURE_MIN_FILTER,gl.NEAREST);
+	// }
+	// configureCubeMap();
+
 
 
 	// obj2 = Square();
@@ -76,6 +118,7 @@ window.onload = function init() {
 	// var m = mult(scalem(30,30,30),rotateX(90));
 	// m = mult(translate(0,-1.0,0), m);
 	// obj2.setModelMatrix(m);
+
 
 
 	// Grid code from previous assignment
