87836a4c45f59d9b2745eacfb49b4404365a0ea9
Simon\GitHub_Desktop <simon.seo@nyu.edu>
2016-12-10 23:03:47 +0400

    add lights, add rotation, add anti-aliasing, add billiard ball texture

--- current/assignment_9/RayTracerFromScratch.js
+++ current/assignment_9/RayTracerFromScratch.js
@@ -1,5 +1,6 @@
 "use strict";
 var gl; // global variable
+var ut;
 
 window.onload = function init(){
   //Set  up WebGL
@@ -26,8 +27,46 @@ window.onload = function init(){
   gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(vPosition);  
 
+  ut = gl.getUniformLocation(program, "time");
+  var diffuseMapSampler = gl.getUniformLocation(program, "diffuseMapSampler")
+  //setupTextureMaps
+  var diffuseMapTexture = setupTexture("billiard_ball_texture.jpg"); 
+  gl.uniform1i(diffuseMapSampler, 0);
+    // we use texture unit 0 for diffuse map
+  gl.activeTexture(gl.TEXTURE0);
+  gl.bindTexture(gl.TEXTURE_2D, diffuseMapTexture);
+  
   //Draw
-  gl.drawArrays(gl.TRIANGLES,0,6); 
+  requestAnimationFrame(render);
 }; 
 
+function render(now) {
+  requestAnimationFrame(render);
+  
+  gl.uniform1f(ut, now/1000);
+  gl.drawArrays(gl.TRIANGLES,0,6);
 
+}
+
+function setupTexture(src){
+  var texture = gl.createTexture();
+  
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE,
+            new Uint8Array([255, 0, 0, 255])); // single red pixel image
+      gl.bindTexture(gl.TEXTURE_2D, null);
+
+  texture.image = new Image();
+  texture.image.onload = handler; 
+  texture.image.src = src;
+
+  function handler(){
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+    gl.generateMipmap(gl.TEXTURE_2D);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+  }
+}
